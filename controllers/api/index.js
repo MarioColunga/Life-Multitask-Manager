bc2f5b733b595d4d509dc0aeed2c4e5eb49760fd
Mariana Romero <51127563+Marian94@users.noreply.github.com>
2022-03-01 21:20:21 -0600

    Feature/login (#12)
    * login backend
    
    * adding prettier and eslint
    
    * adding new changes
    
    Co-authored-by: Mariana Gonzalez <mariana.gonzalez@wizeline.com>

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
-const profilesRoutes = require('./profileRoutes');
-const projectRoutes = require('./projectRoutes');
-const activityRoutes = require('./activityRoutes');
+const router = require("express").Router();
+const profilesRoutes = require("./profileRoutes");
+const userRoutes = require("./userRoutes");
+const projectRoutes = require("./projectRoutes");
+const activityRoutes = require("./activityRoutes");
 
-// Prefix all routes defined in `bookRoutes.js` with `/books
-router.use('/profiles', profilesRoutes);
-router.use('/projects', projectRoutes);
-router.use('/activities', activityRoutes);
+router.use("/profiles", profilesRoutes);
+router.use("/users", userRoutes);
+router.use("/projects", projectRoutes);
+router.use("/activities", activityRoutes);
 
 module.exports = router;
